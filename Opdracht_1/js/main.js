7e74d8746207f99943799ac38328927d97c9af53
Sander Dechering <sanderdechering@Sanders-MacBook-Pro.local>
2018-05-05 17:04:25 +0200

    Settimeout geprobeerd, maar niet uitgekomen.

--- Opdracht_1/js/main.js
+++ Opdracht_1/js/main.js
@@ -2,16 +2,18 @@
 let screenWidth = window.innerWidth
 let screenHeight = window.innerHeight
 
-for (fish = 0; fish < 100; fish++) {
 
+window.setTimeout( function(){for (fish = 0; fish < 100; fish++){
 	let fishElement = document.createElement("fish")
 
 	// Pak breedte en hoogte van de vis. Hoogte is groter dan de vis zelf omdat de vis op en neer beweegt
 	let fishWidth = 130
-	let fishHeight = 200
+	let fishHeight = 180
 
 	// Fish color
-	fishElement.style.backgroundColor = "hsl("+ Math.floor(Math.random() * 360) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1+")"
+	colorCode = Math.floor(Math.random() * 360) + 1
+	fishElement.style.webkitFilter = "hue-rotate(" + colorCode + "deg)"
+	fishElement.style.filter = "hue-rotate(" + colorCode + "deg)"
 
 	// X en Y berekening
 	// We halen fishWidth en Height van screenWidth en height af om te voorkomen dat de vis uit het scherm spawned
@@ -25,13 +27,9 @@ for (fish = 0; fish < 100; fish++) {
 
 	// Fish aan body toevoegen
 	document.body.appendChild(fishElement)
+}},2000)
 
-}
-
-for (bubble = 0; bubble < 100; bubble++) {
-	//
-	// Geef de bubble een random positie
-	//
+for (bubble = 0; bubble < 1; bubble++){
 	let bubbleElement = document.createElement("bubble")
 	let bubbleWidth = 55
 	let bubbleX = (Math.random() * (screenWidth - bubbleWidth)).toFixed()
@@ -39,10 +37,10 @@ for (bubble = 0; bubble < 100; bubble++) {
 	bubbleElement.style.left = bubbleX + "px"
 
 	document.body.appendChild(bubbleElement)
-}
-//
+}	
+
+
 // roep een functie aan als alles geladen is
-//
 window.addEventListener("load", function () {
     console.log("start het aquarium")
 })
